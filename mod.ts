2ea293d22abf232c6184886473ef6da1b31dbace
杨俊宁 <youngjuning@aliyun.com>
2020-08-05 16:13:10 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
 // Copyright 1994-2020 the Duck authors. All rights reserved. MIT license.
 export async function addControllers(router: any, dir: string = "controllers") {
-//   const realPath = Deno.realPathSync(dir);
+  const realPath = Deno.realPathSync(dir);
   for (const dirEntry of Deno.readDirSync(dir)) {
     if (dirEntry.name.endsWith(".ts") || dirEntry.name.endsWith(".js")) {
-      const controller = await import(`${dir}/${dirEntry.name}`);
+      const controller = await import(`${realPath}/${dirEntry.name}`);
       const method = controller.method || "get";
       const api = controller.api ||
       dirEntry.name.replace(/(.*\/)*([^.]+).*/ig, "$2");
