99c2e286dd4d930f27d1990e4e1ce6b4eacf1b97
杨俊宁 <youngjuning@aliyun.com>
2020-08-06 01:55:15 +0800

    修改demo

--- test/abc/server.ts
+++ test/abc/server.ts
@@ -1,8 +1,8 @@
-import { Application,Context } from "https://deno.land/x/abc@v1/mod.ts";
-import duck from '../../mod.ts'
+import { Application } from "https://deno.land/x/abc@v1/mod.ts";
+// import duck from '../../mod.ts'
+import duck from "https://deno.land/x/duck/mod.ts";
 const app = new Application();
 const gaga = await duck(app)
-app.get('/s',(ctx) => {})
 gaga.start({ port: 1998 });
 
 console.log(`🦕 abc server running at http://127.0.0.1:1998/ 🦕`);
