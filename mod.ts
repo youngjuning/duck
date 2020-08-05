bea7c7d27d4c8ccc61668baba390c4b4102fde90
杨俊宁 <youngjuning@aliyun.com>
2020-08-05 16:37:33 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,11 @@
 // Copyright 1994-2020 the Duck authors. All rights reserved. MIT license.
 export async function addControllers(router: any, dir: string = "controllers") {
   const realPath = Deno.realPathSync(dir);
+  router.get("/", (ctx: any) => {
+    ctx.response.body = "Hello Duck!";
+  }).get("/checkHealth", (ctx: any) => {
+    ctx.response.body = "The server is health. Just do it.";
+  });
   for (const dirEntry of Deno.readDirSync(dir)) {
     if (dirEntry.name.endsWith(".ts") || dirEntry.name.endsWith(".js")) {
       const controller = await import(`file://${realPath}/${dirEntry.name}`);
@@ -10,11 +15,6 @@ export async function addControllers(router: any, dir: string = "controllers") {
       router[method](`/${api}`, controller.default);
     }
   }
-  router.get("/", (ctx: any) => {
-    ctx.response.body = "Hello Duck!";
-  }).get("/checkHealth", (ctx: any) => {
-    ctx.response.body = "The server is health. Just do it.";
-  });
   return router;
 }
 
