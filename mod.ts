2227ba0a13281e74175a0079fa70e7f727f9f710
杨俊宁 <youngjuning@aliyun.com>
2020-08-05 14:26:55 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,3 @@
 export function addControllers(router: any, dir: string = 'controllers') {
-    const files = [...Deno.readDirSync(dir)] // 将 Iterator 类型转成数组
-    console.log(files)
+    console.log(Deno.realPathSync(dir));
 }
